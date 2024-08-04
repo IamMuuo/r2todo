1e0b6134a8a38e266b87b32685c1e1d37300430b
Erick <hearteric57@gmail.com>
2024-08-04 22:25:17 +0300

    Batman!

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
-module example.com/packages
+module github.com/iammuuo/r2todo
 
-go 1.19
-
-require github.com/alexeyco/simpletable v1.0.0
+go 1.22.5
 
 require (
-	github.com/mattn/go-runewidth v0.0.12 // indirect
-	github.com/rivo/uniseg v0.1.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,73 +1,22 @@
-//////////////////////////////////////////////////////////////////
-//	Project: Godo
-//
-//	Author:	Erick Muuo
-//	File  : main.go
-//	Description: Contains program entry pint
-//	//////////////////////////////////////////////////////////////
+/*
+Copyright © 2024 Erick Muuo hearteric57@gmail.com
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-
-	"example.com/packages/util"
-)
-
-const (
-	filename string = "Tasks.json"
-)
+import "github.com/iammuuo/r2todo/cmd"
 
 func main() {
-	args := os.Args
-
-	if len(args) <= 1 {
-		fmt.Printf("Usage of: " + args[0] +
-			"\n-add\n\tadd a new todo\n" +
-			"-complete <int>\n\tmark todo as completed\n" +
-			"-delete <int>\n\tdelete a todo\n" +
-			"-list\n\tlist all todos\n")
-
-		os.Exit(1)
-	}
-
-	var t util.Todos
-	t.Load(filename)
-
-	switch args[1] {
-	case "-list":
-		t.Print()
-
-	case "-add":
-		if args[2] != "" {
-			t.Add(args[2])
-		} else {
-			fmt.Println("Use godo -add \"Task\"")
-			os.Exit(1)
-		}
-
-	case "-delete":
-		x, _ := strconv.Atoi(args[2])
-
-		t.Delete(x)
-
-	case "-complete":
-		val, err := strconv.Atoi(args[2])
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		t.Complete(val)
-
-	default:
-		fmt.Printf("Usage of: " + args[0] +
-			"\n-add\n\tadd a new todo\n" +
-			"-complete <int>\n\tmark todo as completed\n" +
-			"-delete <int>\n\tdelete a todo\n" +
-			"-list\n\tlist all todos\n")
-	}
-	t.Store(filename)
+	cmd.Execute()
 }
